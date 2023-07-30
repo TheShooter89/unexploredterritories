af13bf4d091feb31b9e2ee7551ad63c473cf192a
tanque <paoletti.francesco@gmail.com>
2023-07-31 00:30:37 +0200

    (#16) chores(main): clean from useless code

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,6 @@ use pig::PigPlugin;
 mod setup;
 use setup::SetupPlugin;
 
-pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
-pub const RESOLUTION: f32 = 16.0 / 9.0;
-pub const TILE_SIZE: f32 = 0.1;
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Copy)]
-pub enum GameState {
-    StartMenu,
-    Overworld,
-    Interaction,
-}
-
 fn main() {
     credits::print_logo();
     credits::print_credits();
