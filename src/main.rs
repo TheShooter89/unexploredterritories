475ee5910b1036b5d7782c7fa4007e1f415ab0c7
tanque <paoletti.francesco@gmail.com>
2023-07-31 00:27:17 +0200

    (#16) feature(credits): moved credits and logo printing functions to appropriately isolated module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 
 mod configs;
+mod credits;
 
 mod player;
 use player::PlayerPlugin;
@@ -16,39 +17,6 @@ pub const CLEAR: Color = Color::rgb(0.1, 0.1, 0.1);
 pub const RESOLUTION: f32 = 16.0 / 9.0;
 pub const TILE_SIZE: f32 = 0.1;
 
-fn render_logo() {
-    // UNEXPLORED
-    println!("  _    _ _   _ ________   _______  _      ____  _____  ______ _____           ");
-    println!(
-        " | |  | | \\ | |  ____\\ \\ / /  __ \\| |    / __ \\|  __ \\|  ____|  __ \\          "
-    );
-    println!(" | |  | |  \\| | |__   \\ V /| |__) | |   | |  | | |__) | |__  | |  | |         ");
-    println!(" | |  | | . ` |  __|   > < |  ___/| |   | |  | |  _  /|  __| | |  | |         ");
-    println!(" | |__| | |\\  | |____ / . \\| |    | |___| |__| | | \\ \\| |____| |__| |         ");
-    println!(
-        "  \\____/|_| \\_|______/_/ \\_\\_|    |______\\____/|_|  \\_\\______|_____/          "
-    );
-
-    // TERRITORIES
-    println!("  _______ ______ _____  _____  _____ _______ ____  _____  _____ ______  _____ ");
-    println!(" |__   __|  ____|  __ \\|  __ \\|_   _|__   __/ __ \\|  __ \\|_   _|  ____|/ ____|");
-    println!("    | |  | |__  | |__) | |__) | | |    | | | |  | | |__) | | | | |__  | (___  ");
-    println!("    | |  |  __| |  _  /|  _  /  | |    | | | |  | |  _  /  | | |  __|  \\___ \\ ");
-    println!(
-        "    | |  | |____| | \\ \\| | \\ \\ _| |_   | | | |__| | | \\ \\ _| |_| |____ ____) |"
-    );
-    println!(
-        "    |_|  |______|_|  \\_\\_|  \\_\\_____|  |_|  \\____/|_|  \\_\\_____|______|_____/ "
-    );
-}
-
-fn render_credits() {
-    println!("----------------------");
-    println!("dedicated to A.");
-    println!("made with <3 by tanque");
-    println!("----------------------");
-}
-
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Copy)]
 pub enum GameState {
     StartMenu,
@@ -57,8 +25,8 @@ pub enum GameState {
 }
 
 fn main() {
-    render_logo();
-    render_credits();
+    credits::print_logo();
+    credits::print_credits();
 
     App::new()
         .add_plugins(
